97210a11c34ec24924abe1ef5cde207636f1794a
AsamShanBD <asam.shan@bradbourne.digital>
2020-01-14 12:34:12 +0000

    updating config

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dotfiles
+
+Repo containing various configs for apps I use on a daily basis, comes in handy when explaining my setup to others or setup on a new computer.
---
+++ vscode.js
@@ -0,0 +1,61 @@
+{
+  "editor.fontFamily": "Hack",
+  "editor.detectIndentation": true,
+  "editor.lightbulb.enabled": false,
+  "editor.fontSize": 22,
+  "editor.lineHeight": 35,
+  "editor.quickSuggestionsDelay": 30,
+  "editor.letterSpacing": 0.5,
+  "workbench.editor.tabSizing": "shrink",
+  "html.format.enable": true,
+  "html.format.preserveNewLines": true,
+  "files.trimTrailingWhitespace": true,
+  "editor.tabSize": 2,
+  "editor.cursorWidth": 5,
+  "editor.cursorStyle": "line",
+  "editor.wordWrap": "on",
+  "editor.multiCursorModifier": "ctrlCmd",
+  "editor.fontWeight": "400",
+  "editor.cursorBlinking": "solid",
+  "editor.minimap.enabled": false,
+  "workbench.sideBar.location": "left",
+  "editor.minimap.showSlider": "always",
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "[javascript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  "spellright.language": [
+    "en"
+  ],
+  "spellright.documentTypes": [
+    "markdown",
+    "plaintext",
+    "mdx"
+  ],
+  "spellright.parserByClass": {
+    "mdx": {
+      "parser": "markdown"
+    }
+  },
+  "editor.renderWhitespace": "boundary",
+  "editor.cursorSmoothCaretAnimation": true,
+  "editor.smoothScrolling": true,
+  "editor.snippetSuggestions": "top",
+  "editor.formatOnSave": true,
+  "editor.autoIndent": true,
+  "files.insertFinalNewline": true,
+  "editor.matchBrackets": true,
+  "window.openFilesInNewWindow": "off",
+  "workbench.editor.enablePreview": true,
+  "editor.scrollBeyondLastLine": true,
+  "editor.formatOnPaste": true,
+  "window.zoomLevel": 0,
+  "editor.parameterHints.enabled": false,
+  "terminal.integrated.fontSize": 18,
+  "workbench.colorTheme": "Dracula",
+  "editor.suggestSelection": "first",
+  "vsintellicode.modify.editor.suggestSelection": "automaticallyOverrodeDefaultValue"
+}
